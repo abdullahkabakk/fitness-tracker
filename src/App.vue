a1ca0d960d8475b3d849b906c518f469c0899b86
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2025-04-15 12:12:37 +0300

    feat: add dynamic layout

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <RouterView />
+  <div class="flex flex-col max-w-[100%] w-full min-h-screen bg-background-light dark:bg-background-dark">
+    <component :is="layoutComponent" v-bind="props">
+      <RouterView />
+    </component>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+// Dynamically select the layout component from the route's meta
+const route = useRoute();
+const layoutComponent = computed(() => route.meta.layoutComponent);
+const props = computed(() => route.meta.props);
+</script>
